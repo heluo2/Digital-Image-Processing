e177362d3414f77c85f65a923120ac5fa2101243
heluo2 <80439910+heluo2@users.noreply.github.com>
2021-10-14 20:12:44 +0800

    Add files via upload

---
+++ 03.Fundamentals of Digital Image Processing/3.1 Overview of Digital Image Processing.py
@@ -0,0 +1,20 @@
+'''
+
+数字图像处理又称为计算机图像处理，是指将图像信号转换为数字信号并利用计算机对其进行处理的过程。
+数字图像处理的产生和发展主要受三个因素的影响：
+一：计算机的发展；
+二：数学的发展（特别是离散数学理论的创立和发展）
+三：应用需求的增长
+
+常见的数字图像处理方法：
+1.算术处理(arithmetic processing)
+2.集合处理(geometrical processing)
+3.图像增强(image enhancement)
+4.图像识别(image recognition)
+5.图像分类(image classification)
+6.图像复原(image restoration)
+7.图像重建(image reconstruction)
+8.图像编码(image encoding)
+9.图像理解(image understanding)
+
+'''
